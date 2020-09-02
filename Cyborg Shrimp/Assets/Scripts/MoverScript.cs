5dfaa9be3ade9bd0544268507903ea8c01905b76
Carissa <carissasunny@gmail.com>
2020-09-01 20:14:49 -0600

    unity update

---
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message: "Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(1 * Time.deltaTime, 0, 0);
+        
+    }
+}

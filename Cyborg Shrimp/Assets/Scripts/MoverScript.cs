9ca26d18785c6ce5f1dd64dd019b98d77b82801d
Carissa <carissasunny@gmail.com>
2020-09-03 16:44:47 -0600

    unity

--- Cyborg Shrimp/Assets/Scripts/MoverScript.cs
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -9,11 +9,37 @@ public class MoverScript : MonoBehaviour
     {
         Debug.Log(message: "Hello World!");
     }
+    public float speed = 5f;
+    //variable is "speed"
+    public int score = 100; 
+    // int must be a whole interger
+    public float Health = 50.0f;
+    public string Password;
+    public int Points;
+    public string playerName;
+
+    
 
     // Update is called once per frame
-    void Update()
+    //private means it cannot be seen from the Unity editor, as if you had your blinds closed
+    private void Update()
     {
-        transform.Translate(1 * Time.deltaTime, 0, 0);
+        // var declares variables inside a function
+        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(hInput, vInput, 0);
+        // the "." accesses the modifiers inside the class (transform or Time in this case)
+  
+    }
+
+    public void Up()
+    {
+        Debug.Log(message: "what did you think this was going to do?");
+    }
+    
+    public void Down()
+    {
         
+        Debug.Log(message: "what did you think this was going to do?");
     }
 }

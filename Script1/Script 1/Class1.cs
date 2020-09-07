590ca498d2b86d4e138f5429cdd080e512f62d94
Carissa <carissasunny@gmail.com>
2020-09-07 15:22:08 -0600

    added basic script

---
+++ Script1/Script 1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Script_1
+{
+    public class PlayerHealth
+    {
+        private int hp1;
+        private int hp2;
+        string player1;
+        string player2;
+    }
+    public class PlayerScore
+    {
+        private int score1;
+        private int score2;
+        public string password = "12345";
+    }
+    public class CharacterMover
+    {
+        private float speed;
+        
+    }
+}

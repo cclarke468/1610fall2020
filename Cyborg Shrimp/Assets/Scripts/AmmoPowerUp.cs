f07cd677eed2b83293e59fe13ea53ec90fffaa58
Carissa <carissasunny@gmail.com>
2020-09-12 22:19:27 -0600

    shrimp
    added powerups

---
+++ Cyborg Shrimp/Assets/Scripts/AmmoPowerUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPowerUp : MonoBehaviour
+{
+    public int ammo;
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+        print(ammo);
+    }
+}

b551148920aec67b9899a905dd6b5d2ed4a804c6
Carissa <carissasunny@gmail.com>
2020-09-17 15:19:43 -0600

    shrimp

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public UnityEvent powerUpEvent;
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
+        //in this case, invoke makes power up object disappear!
     }
 }
---
+++ Cyborg Shrimp/Assets/Scripts/UnityEvents.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEvents : MonoBehaviour
+{
+   public UnityEvent myEvent;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      myEvent.Invoke();
+      //invoke is a function to tell the event to run
+      //sets GameObject setActive (in Unity Editor) to false
+   }
+}

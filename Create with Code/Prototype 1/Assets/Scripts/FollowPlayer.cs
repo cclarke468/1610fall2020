8a2a0096b6fe7ed9517e7b0fd8e793d92f774db7
Carissa <carissasunny@gmail.com>
2020-09-21 13:00:55 -0600

    cwc 1.3 done

---
+++ Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //this is to reference the vehicle/player in the game—drag and drop the vehicle into this slot in the inspector
+    public GameObject player;
+   
+    //this part takes place after the **
+    public Vector3 offset = new Vector3(0, 5, -9);
+    void Update()
+    {
+        //--transform.position = player.transform.position;
+        //we need to offset the camera behind player by adding to player position
+        //"new" creates new instance (different from a variable) of a vector3; it is TEMPORARY!
+        //--transform.position = player.transform.position + new Vector3(0, 5, -9);
+        //**dang it...we're making a permanent variable for the new vector3 called offset
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        //Move vehicle forward
+        //transform.Translate(0,0,1); OR...
+        //transform.Translate(Vector3.forward);
+        //transform.Translate(Vector3.forward * Time.deltaTime * 20); //Time.deltatime makes vehicle update every second instead of every frame (like Update() does)
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}

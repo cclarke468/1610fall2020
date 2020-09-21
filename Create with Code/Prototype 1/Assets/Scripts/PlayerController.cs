4904d060f9d89369bdb65398448a544a336427eb
Carissa <carissasunny@gmail.com>
2020-09-21 15:08:29 -0600

    cwc 1.4 done

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    void Start()
-    {
-        
-    }
-    
+    private float speed = 20f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
+    //fun fact: you can edit unity's input info by going to edit>project settings>input manager
     void Update()
     {
-        //Move vehicle forward
-        //transform.Translate(0,0,1); OR...
-        //transform.Translate(Vector3.forward);
-        //transform.Translate(Vector3.forward * Time.deltaTime * 20); //Time.deltatime makes vehicle update every second instead of every frame (like Update() does)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //getting player input...
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        //Time.deltaTime makes vehicle update every second instead of every frame
+        
+        //move vehicle forward based on forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       
+        //rotates car based on horizontal input
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
+
     }
 }

fff2fb2bd2924643da04a36d186d72bea5c540db
Carissa <carissasunny@gmail.com>
2020-09-28 23:59:04 -0600

    cwc 2.1

--- Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput, speed = 10f;
+    private float xRange = 10f;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        //keep player in bounds
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         
     }
 }

bff55ea44bf05952579b4879dc993ed7e1a7a63a
Carissa <carissasunny@gmail.com>
2020-11-02 23:16:28 -0700

    cwc
    sorry i'm behind, ive been sick

---
+++ Create with Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+    }
+}

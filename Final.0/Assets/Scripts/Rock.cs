144915a9167aaf6bcfd3a5697221c255c5bcb5e3
Carissa <carissasunny@gmail.com>
2020-11-16 23:49:47 -0700

    final

--- Final.0/Assets/Scripts/Rock.cs
+++ Final.0/Assets/Scripts/Rock.cs
@@ -7,21 +7,24 @@ using Random = UnityEngine.Random;
 public class Rock : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private GameManager gameManager;
+    // private GameManager gameManager;
     public int pointValue;
     public ParticleSystem explosionParticle;
+    public GlobalData globalData;
     void Start()
     {
         targetRb = gameObject.GetComponent<Rigidbody>();
         
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        // gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
-        if (!gameObject.CompareTag("Bad"))
+        if (!gameObject.CompareTag("Rock")) //REPLACE TAG with better identification system
         {
-            gameManager.GameOver();
+            // gameManager.GameOver();
+            globalData.GameOver();
+            globalData.UpdateScore(3);
         }
     }
     void Update()

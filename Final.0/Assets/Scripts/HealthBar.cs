feee00117f811b14662433e73e0fa8899a3093ae
Carissa <carissasunny@gmail.com>
2020-12-04 01:58:19 -0700

    final

---
+++ Final.0/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class HealthBar : MonoBehaviour
+{
+    private Image o2Bar;
+    private void Start()
+    {
+        o2Bar = GetComponent<Image>();
+    }
+
+    // public void DisplayValue(FloatData data)
+    // {
+    //     o2Bar.fillAmount = data.value;
+            //need coroutine for slow decrease of O2
+    // }
+}
